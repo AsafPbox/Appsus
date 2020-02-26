22805105c8fbda4a7d075b01da636499ebb6f04b
AsafP <asafp.box@gmail.com>
2020-02-26 16:10:39 +0200

    a3

--- js/apps/email/email-list.cmp.js
+++ js/apps/email/email-list.cmp.js
@@ -4,23 +4,23 @@ export default {
     template: `
     <section class="emails-container">
         <ul>
-            <li v-for="email in emails" @click="printEmail(email)">
+            <li v-for="email in emails" @click=printEmail(email)>
                 {{email.sender}}
                 |
                 {{email.subject}}
-                <email-preview v-show="isClicked" :email="email"></email-preview>
+                <email-preview v-show="" :email="email"></email-preview>
             </li>  
         </ul>
     </section>
     `,
     data (){
         return {
-            isClicked: false
+            isClicked: false,
         }
     },
     methods: {
         printEmail(email){
-            console.log(email.id)
+            console.log(email)
         }
     },
     props: ['emails'],
