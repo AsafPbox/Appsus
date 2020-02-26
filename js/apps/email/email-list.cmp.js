2b5996e170c898de68a1f11b49d08f51c4510d0b
AsafP <asafp.box@gmail.com>
2020-02-26 15:36:16 +0200

    a2

--- js/apps/email/email-list.cmp.js
+++ js/apps/email/email-list.cmp.js
@@ -4,31 +4,27 @@ export default {
     template: `
     <section class="emails-container">
         <ul>
-            <div @click=printEmail()>
-            <li v-for="(email) in emails" >
+            <li v-for="email in emails" @click="printEmail(email)">
                 {{email.sender}}
                 |
                 {{email.subject}}
-                <!-- <email-preview v-show="isClicked" :email="email"></email-preview> -->
-                <!-- <button @click="$emit('remove', currCar.id)">x</button> -->
+                <email-preview v-show="isClicked" :email="email"></email-preview>
             </li>  
-            </div>      
         </ul>
     </section>
     `,
     data (){
         return {
-            isClicked: false 
+            isClicked: false
+        }
+    },
+    methods: {
+        printEmail(email){
+            console.log(email.id)
         }
     },
     props: ['emails'],
     components: {
         emailPreview
-    },
-    methods: {
-        printEmail(){
-            // this.isClicked = !this.isClicked
-            console.log()
-        }
     }
 }
--- js/apps/email/email-preview.cmp.js
+++ js/apps/email/email-preview.cmp.js
@@ -1,8 +1,8 @@
 export default {
     template: `
         <section>
-            <h3>{{email.sender}}</h3>
-            <h4>{{email.subject}}</h4>
+            <h3>{{email.id}}</h3>
+            <h3>{{email.body}}</h3>
             <h4>{{email.sentAt}}</h4>
         </section>               
     `,
