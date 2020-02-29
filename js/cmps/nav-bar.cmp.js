e335ab0beef4a769a9613362132801d69d519662
Nir Gonen <gonen.nir@gmail.com>
2020-02-29 13:50:04 +0200

    Friday updates

--- js/cmps/nav-bar.cmp.js
+++ js/cmps/nav-bar.cmp.js
@@ -2,26 +2,30 @@ export default {
 	template: `
     <section>
         <nav class="flex space-between">
-    <div class="logo-name">Apps Place</div>
-    <ul class="nav-bar-list flex clean-list">
+        <div class="logo-name">
+        <router-link to="/">
+        <img src="img/nav-bar/sus.svg" title="AppSus" /> AppsSUS
+        </router-link>
+        </div>
+        <ul class="nav-bar-list flex clean-list">
         <li>
             <router-link to="/">
-                Home
-            </router-link>
-        </li>
-        <li>
-            <router-link to="/books">
-                Books
+                <img src="img/nav-bar/home.svg" title="Home" /> Home
             </router-link>
         </li>
         <li>
             <router-link to="/email">
-                Email
+                <img src="img/nav-bar/mail.svg" title="Mail" /> Mail
+            </router-link>
+            </li>
+        <li>
+            <router-link to="/keep">
+                <img src="img/nav-bar/keep.svg" title="Keep" /> Keep
             </router-link>
         </li>
         <li>
-            <router-link to="/keep">
-                Keep
+            <router-link to="/books">
+                <img src="img/nav-bar/books.svg" title="Books" /> Books
             </router-link>
         </li>
     </ul>    
--- js/pages/home.cmp.js
+++ js/pages/home.cmp.js
@@ -1,5 +1,21 @@
 export default {
     template: `
-        <h1>Home</h1>
+        <section class="home-container flex">
+            <div>
+            <router-link to="/email">
+                <img src="img/home/mail.png" title="Mail" />
+            </router-link>
+            </div>
+            <div>
+            <router-link to="/keep">
+                <img src="img/home/keep.png" title="Keep" />
+            </router-link>
+            </div>
+            <div>
+            <router-link to="/books">
+                <img src="img/home/books.png" title="Books" />
+            </router-link>
+            </div>
+        </section>
     `
 }
